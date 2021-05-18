e03e33942b1c4d4d5bf376ec42f61132e80e67b4
NathanSilvaSantos <nathan013silva@gmail.com>
2021-05-17 22:56:18 -0300

    utilizando asterisco

---
+++ regex/othersCaracters.py
@@ -0,0 +1,18 @@
+import re
+
+# Podemos utilizar o caractere ? para marcar um grupo
+# como sendo uma parte opicional do padrão buscado
+batRegex = re.compile(r'Bat(wo)?man')
+# o caractere sempre vai se referir ao grupo que antecede a ele
+# comoo sendo um parametro opicional dentro do padrão
+batman = batRegex.search("Batman begins")
+print(batman.group())
+
+batwoman = batRegex.search('The adventures of Batwoman')
+print(batwoman.group())
+
+# Usando * temos o mesmo efeito, porém ele se aplica caso o grupo
+# apareça mais vezes no texto
+newBatRegex = re.compile(r'Bat(wo)*man')
+mo = newBatRegex.search('The adventures of Batwowowowoman')
+print(mo.group())
